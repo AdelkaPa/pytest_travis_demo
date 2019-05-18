90e8fc5ea391454cc612590dbb8c6c93037c8b48
AdelkaPa <adeeova@gmail.com>
2019-05-18 16:24:57 +0200

    for fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def fizzbuzz(n):
+    if not isinstance(n, int):
+        raise TypeError("n is not an int")
+    if n <= 0:
+        raise ValueError("n is not positive")
+    if n % 3 == 0:
+        return 'fizz'
+    if n % 5 == 0:
+        return 'buzz'
+    if n % 15 == 0:
+        return 'fizzbuzz'
+    return str(n)
+    
+
+
